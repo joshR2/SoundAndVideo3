a6190facd5fb96c3a3afd0f4b0c272a50d35e71c
joshR2 <josh2cool365@aol.com>
2015-11-10 09:51:49 -0700

    Declared some Variables

--- app/src/main/java/com/example/jrob2049/soundandvideo3/SoundActivity.java
+++ app/src/main/java/com/example/jrob2049/soundandvideo3/SoundActivity.java
@@ -2,8 +2,21 @@ package com.example.jrob2049.soundandvideo3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.view.View;
+import android.widget.*;
 
-public class SoundActivity extends AppCompatActivity {
+public class SoundActivity extends Activity implements Runnable
+{
+    private Button startButton;
+    private Button stopButton;
+    private Button pauseButton;
+    private Button videButton;
+    private MediaPlayer soundPlayer;
+    private SeekBar soundBar;
+    private Thread soundthread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
