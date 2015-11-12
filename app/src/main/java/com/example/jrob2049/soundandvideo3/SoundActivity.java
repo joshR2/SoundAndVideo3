90492d36984476ad71e773217fc0fe5c5b7721d5
joshR2 <josh2cool365@aol.com>
2015-11-12 09:56:03 -0700

    Created onClickListener method

--- app/src/main/java/com/example/jrob2049/soundandvideo3/SoundActivity.java
+++ app/src/main/java/com/example/jrob2049/soundandvideo3/SoundActivity.java
@@ -13,14 +13,100 @@ public class SoundActivity extends Activity implements Runnable
     private Button startButton;
     private Button stopButton;
     private Button pauseButton;
-    private Button videButton;
+    private Button videoButton;
     private MediaPlayer soundPlayer;
     private SeekBar soundBar;
     private Thread soundthread;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound);
+
+        startButton = (Button) findViewById(R.id.playButton);
+        stopButton = (Button) findViewById(R.id.stopButton);
+        pauseButton = (Button) findViewById(R.id.pauseButton);
+        videoButton = (Button) findViewById(R.id.videoButton);
+        soundPlayer = MediaPlayer.create(this.getBaseContext(), R.raw.imagineDragons);
+
+        setupListeners();
+
+        soundThread = new Thread(this);
+        soundThread.start();
+
+
     }
+
+    private void setupListenders()
+    {
+        startButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                soundPlayer.start();
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                soundPlayer.pause();
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener()
+        {
+           @Override
+            public void onClick(View currentView)
+           {
+               soundPlayer.stop();
+               soundPlayer = MediaPlayer.create(getBaseContext(), R.raw.imagineDragons);
+           }
+        });
+
+        videoButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View currentView)
+            {
+                Intent myIntent = new Intent(currentView.getContext(),VideoActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
+
+        soundBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onStopTrackingtouch(SeekBar seekBar)
+            {}
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {}
+
+            @Override
+            public void onProgressChjanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                if(fromUser)
+                {
+                    soundPlayer.seekTo(progress);
+                }
+
+            }
+
+
+        });
+    }
+
+    /**
+     * Required since we are implementing Runnable.
+     */
+
+
 }
+
